99fd8e715cc297c9d4d088cf5374488cf70e08da
Cody <cody@bitaccel.com>
2024-01-26 19:34:33 +0100

    remove snapshit id, improve random gen, improve logging

--- shuffler.py
+++ shuffler.py
@@ -8,7 +8,11 @@ from spotipy.oauth2 import SpotifyOAuth
 
 def main():
     log = logging.getLogger('shuffler')
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(name)s] %(levelname)s %(message)s')
+    logFormatter = logging.Formatter('%(asctime)s [%(name)s] %(levelname)s %(message)s')
+    logHandler = logging.FileHandler(filename='shuffler.log', mode='a')
+    logHandler.setFormatter(logFormatter)
+    log.addHandler(logHandler)
+    log.setLevel(logging.DEBUG)
     load_dotenv()
     scope = "playlist-modify-private,playlist-modify-public,playlist-read-private,playlist-read-collaborative"
     spotify = Spotify(auth_manager=SpotifyOAuth(scope=scope))
@@ -18,11 +22,11 @@ def main():
         playlist = spotify.playlist(playlist_id, fields="name,tracks")
         playlist_length = playlist["tracks"]["total"]
         log.info(f'shuffling: {playlist["name"]} with {playlist_length} tracks')
-        snapshot_id = None
+        r = [ random.randrange(playlist_length - 1) for i in range(playlist_length)]
+        log.debug(f'generated random list: {r}')
         for i in range(playlist_length):
-            random_item = random.randrange(playlist_length - 1)
-            random_position = random.randrange(playlist_length - 1)
-            snapshot_id = spotify.playlist_reorder_items(playlist_id, range_start=random_item, range_length=1, insert_before=random_position, snapshot_id=snapshot_id)["snapshot_id"]
+            result = spotify.playlist_reorder_items(playlist_id, range_start=i, range_length=1, insert_before=r[i])
+            log.debug(f'playlist: {playlist["name"]} position: {i} new position: {r[i]}')
 
 if __name__ == "__main__":
     main()
